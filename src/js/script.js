4d6f153fb843d13cc429f565b919d87e477531de
Ari <aaa100107aaa@gmail.com>
2022-06-18 14:03:37 +0600

    added file readme.md

---
+++ src/js/script.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items:7,
+        loop: true,
+        autoplay: true,
+        autoplayHoverPause: true,
+        speed: 100,
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
